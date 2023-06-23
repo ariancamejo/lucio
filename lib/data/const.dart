a6b8f4e76924201a4f62c67b4c7958017e8ce366
Arian Camejo Carrera <57977658+ariancamejo@users.noreply.github.com>
2023-06-23 14:15:48 -0400

    Update const.dart

--- lib/data/const.dart
+++ lib/data/const.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const kAppName = 'Sales-Production';
+const kAppName = 'Lucio';
 const kDefaultRefNumber = 16.0;
 const kGradientColors = [Colors.blue, Colors.lightBlueAccent];
 const dateFormat = 'dd-MMMM-yyyy';
